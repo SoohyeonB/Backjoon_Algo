4e5180fe88bdbef0615b6587d1a8baca014c129f
ewha-coder <56013677+ewha-coder@users.noreply.github.com>
2021-03-23 19:17:35 +0900

    재귀 lotto 6603 문제

---
+++ BackJoon_FirstWeek/Lotto_6603.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*재귀문제 - 6603 로또 */
+//1. 1-49 중 k개를 뽑아 s만들기
+//2. s중에서 6개씩 뽑아 가능한 집합의 개수 구하기
+// DFS활용
+
+int arr[13];
+int answer[14];
+int k;
+
+void DFS(int start, int depth) {
+	int i;
+	if (depth == 6) {	
+		for (i = 0; i < 6; i++)
+			printf("%d ", answer[i]);
+		printf("\n");
+	}
+	else {
+		for (i = start; i < k; i++) {
+			answer[depth] = arr[i];
+			DFS(i + 1, depth + 1);
+		}
+	}
+}
+
+
+void main() {
+	int i;
+	while (1) {
+		scanf_s("%d", &k);
+		if (k == 0) break;
+		for (i = 0; i < k; i++)
+			scanf_s("%d", &arr[i]);  //k개의 숫자 입력
+		DFS(0, 0);
+		printf("\n");
+	}
+}
